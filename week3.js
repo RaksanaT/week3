9a0593369dd5bd3709355bc6a854de66df1153ad
RoxannaTp <roxanataptygina@yahoo.com>
2022-10-29 19:11:19 -0400

    1st task

--- week3.js
+++ week3.js
@@ -3,6 +3,7 @@
 // •	Do not use numbers to reference the last element, find it programmatically, 
 // •	ages[7] – ages[0] is not allowed!
 var ages = [3, 9, 23, 64, 2, 8, 28, 93];
+console.log(ages[ages.length - 1] - ages[0]);
 
 // 1b.	Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
 // 1c.	Use a loop to iterate through the array and calculate the average age. 
